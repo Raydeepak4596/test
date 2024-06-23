e54c5f0ec5a7a0a74a6fda9df95ceab38691b690
deepak <raydeepak4596@gmail.com>
2024-06-23 12:29:48 +0530

    second commit

--- myapp/src/Pages/Register.jsx
+++ myapp/src/Pages/Register.jsx
@@ -70,7 +70,7 @@ return (
         <tr><input className='w-full' type="text" name='username' value={data.username} onChange={handlechange} placeholder='Username' /></tr>
         <tr><input className='w-full' type="text" name='password' value={data.password} onChange={handlechange} placeholder='Password' /></tr>
         <tr><input className='w-full' type="text" name='conferm_password' value={data.conferm_password} onChange={handlechange} placeholder='Conferm Password' /></tr>
-        <tr><button onClick={registerbtn} type='button' className='w-full bg-green-500'>Register</button></tr>
+        <tr><button onClick={registerbtn} type='button' className='w-full bg-green-400'>Register</button></tr>
         <tr><button onClick={loginbtn}>Login</button></tr>
       </table>
       {/* <h2>{test1}</h2> */}
